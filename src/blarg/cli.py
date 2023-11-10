bb97f6520d7c9e05cebcc4cc2a2e25f025ed0dd1
Daniel Roy Greenfeld <62857+pydanny@users.noreply.github.com>
2023-11-10 17:16:04 +0000

    Get index page working

--- src/blarg/cli.py
+++ src/blarg/cli.py
@@ -22,9 +22,11 @@ def serve(source: Path, target: Path):
     try:
         shutil.rmtree(target)
     except FileNotFoundError:
-        pass
+        print("Target directory does not exist.")
 
-    print(f"Building {source}")
+    print(f"Building {target}")
+    articles = []
+
     for page in source.rglob('*/*.md'):
         # print(page)
         try:
@@ -33,12 +35,23 @@ def serve(source: Path, target: Path):
             continue
         frontmatter = segmented_page['frontmatter']
         content = segmented_page['content']
-        # print(page.name)
-        target_file = target / Path(page.name).with_suffix('.html')
+        
+        articles.append(target / Path(page.name).with_suffix(''))
+        target_file = target / Path(page.name).with_suffix('') / Path('index.html')
         target_file.parent.mkdir(parents=True, exist_ok=True)        
 
         html = markdown.markdown(content, extensions=['pymdownx.magiclink'])
         target_file.write_text(html)
+
+    # Index
+    index = "<ul>"
+    for article in articles:
+        print(article)
+        link = article.relative_to(target)
+        index += f'\n<li><a href="{link}">{article}</a></li>'
+    index += "</ul>"
+    target_index = target / 'index.html'
+    target_index.write_text(index)
 
 if __name__ == "__main__":
     app()
